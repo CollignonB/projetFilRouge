42fd5e3b01f676ceb18273d25c2e1f5bc1aa7cb0
Baptiste COLLIGNON <collignon@CDI>
2020-10-13 16:29:43 +0200

    display lastOp functionnal but none mcv friendly

--- index.php
+++ index.php
@@ -13,18 +13,18 @@ if(empty($_SESSION["user"]) || !isset($_SESSION["user"])){
 $account = get_accounts($db, $_SESSION["user"]["id"]);
 
 // gérer le problème de cette requête, elle renvoit le même montant sur tout les comptes 
-// $query = $db->prepare(
-//   "SELECT t.type, t.amount, t.date_transfert FROM `transferts` as t 
-//   INNER JOIN accounts as a ON a.id = t.account_id 
-//   INNER JOIN account_types as a_t ON a_t.id = a.account_type_id 
-//   WHERE a.id = :userId 
-//   ORDER BY date_transfert DESC
-//   LIMIT 1;"
-// );
+$query = $db->prepare(
+  "SELECT t.type, t.amount, t.date_transfert FROM `transferts` as t 
+  INNER JOIN accounts as a ON a.id = t.account_id 
+  INNER JOIN account_types as a_t ON a_t.id = a.account_type_id 
+  WHERE a.id = :accountId 
+  ORDER BY date_transfert DESC
+  LIMIT 1;"
+);
 
 // $query->execute([
-//   "userId" => $_SESSION["user"]["id"]
-// ]);~
+//   "accountId" => $value["id"]
+// ]);
 // $data = $query->fetch(PDO::FETCH_ASSOC);
 // $lastOp = $data["type"] . "/" . $data["amount"] . "€</br>" . $data["date_transfert"];
 
@@ -38,7 +38,12 @@ $account = get_accounts($db, $_SESSION["user"]["id"]);
     <div class="row">
 
     <?php 
-    foreach($account as $key => $value):  
+    foreach($account as $key => $value): 
+      $query->execute([
+        "accountId" => $value["id"]
+      ]);
+      $data = $query->fetch(PDO::FETCH_ASSOC);
+      // $lastOp = $data["type"] . "/" . $data["amount"] . "€</br>" . $data["date_transfert"];
     ?>
 
       <div class="card mb-4 col-lg-3 mr-4 col-sm-12 col-md-5">
@@ -52,10 +57,10 @@ $account = get_accounts($db, $_SESSION["user"]["id"]);
           </button>
           <p class="card-text collapse" id="montantCible<?php echo $value["id"]?>"><?php echo $value['montant']; ?>€</p>
 
-          <!-- <button class="btn btn-primary mb-2" type="button" data-toggle="collapse" data-target="#operationCible<?php echo $value["id"]?>" aria-expanded="false" aria-controls="collapseExample">
+          <button class="btn btn-primary mb-2" type="button" data-toggle="collapse" data-target="#operationCible<?php echo $value["id"]?>" aria-expanded="false" aria-controls="collapseExample">
             Dernière opération 
           </button>
-          <p class="card-text collapse" id="operationCible<?php echo $value["id"]?>"><?php echo $lastOp ?></p> -->
+          <p class="card-text collapse" id="operationCible<?php echo $value["id"]?>"><?php echo $data["type"] . "/" . $data["amount"] . "€</br>" . $data["date_transfert"] ?></p>
           <a class="btn btn-primary" href="mouvement.php?id=<?php echo $value['id']; ?>">Dépôt/Retrait</a>
           <button class="btn btn-danger mt-2" onclick="deleteAccount()">Supprimer le compte</button>
         </div>
